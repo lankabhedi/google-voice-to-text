3f576bd5d7f1d5ee57a9b11d2ece8f41ae28dc67
lankabhedi <50140975+lankabhedi@users.noreply.github.com>
2021-06-15 04:45:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request
 from boltiot import Bolt
 import time as t
 
-app = Flask(__name__, template_folder="./templates")
+app = Flask(__name__, template_folder="templates/")
 
 @app.route('/', methods = ['GET','POST'])
 def mainpage():
